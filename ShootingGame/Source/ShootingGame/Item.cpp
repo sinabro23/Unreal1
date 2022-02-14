fd01c3b069abe78a52aaef5ccffda5d67778f029
jinabro <kmy4869@naver.com>
2022-02-14 21:38:21 +0900

    총 래리티 별개수

--- ShootingGame/Source/ShootingGame/Item.cpp
+++ ShootingGame/Source/ShootingGame/Item.cpp
@@ -7,7 +7,10 @@
 #include "Components/SphereComponent.h"
 #include "Murdoc.h"
 // Sets default values
-AItem::AItem()
+AItem::AItem() :
+	ItemName(FString("Default")),
+	ItemCount(0),
+	ItemRarity(EItemRarity::EIR_Common)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -34,6 +37,9 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 	PickupWidget->SetVisibility(false);
+
+	// 별 개수 몇개 띄울지 정해줌(레어도에 따라)
+	SetActiveStars();
 
 	// AreaSphere를 위한 오버랩 셋업
 	// 바인드 한 함수를 오버램 될때 실행 시켜줌
@@ -64,6 +70,51 @@ void AItem::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor*
 		}
 	}
 }
+
+
+void AItem::SetActiveStars()
+{
+	// 0인덱스를 사용하지 않기위해 별은 5개지만 6개 만든것
+	for (int32 i = 0; i <= 5; i++)
+	{
+		ActiveStars.Add(false); // 6개 false가 들어감.
+	}
+
+	switch (ItemRarity)
+	{
+	case EItemRarity::EIR_Damaged:
+		ActiveStars[1] = true;
+		break;
+	case EItemRarity::EIR_Common:
+		ActiveStars[1] = true;
+		ActiveStars[2] = true;
+		break;
+	case EItemRarity::EIR_UnCommon:
+		ActiveStars[1] = true;
+		ActiveStars[2] = true;
+		ActiveStars[3] = true;
+		break;
+	case EItemRarity::EIR_Rare:
+		ActiveStars[1] = true;
+		ActiveStars[2] = true;
+		ActiveStars[3] = true;
+		ActiveStars[4] = true;
+		break;
+	case EItemRarity::EIR_Legendary:
+		ActiveStars[1] = true;
+		ActiveStars[2] = true;
+		ActiveStars[3] = true;
+		ActiveStars[4] = true;
+		ActiveStars[5] = true;
+		break;
+	case EItemRarity::EIR_Max:
+		break;
+	default:
+		break;
+	}
+}
+
+
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
